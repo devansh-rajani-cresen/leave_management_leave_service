c92fd999d7d9875c4f28b0c131a3972dab37ae9c
DevanshCresen <devansh.rajani@cresensolutions.com>
2026-05-18 10:49:35 +0530

    feat: Public holiday controller added for ADMIN

--- src/main/java/com/cresensolutions/leaveservice/controller/PublicHolidayController.java
+++ src/main/java/com/cresensolutions/leaveservice/controller/PublicHolidayController.java
@@ -1,4 +1,41 @@
 package com.cresensolutions.leaveservice.controller;
 
 public class PublicHolidayController {
+
+    private final PublicHolidayService publicHolidayService;
+
+    // Available for all employees
+    @GetMapping("/public-holidays")
+    public List<PublicHolidayResponse> getAllHolidays() {
+        return publicHolidayService.getAllHolidays();
+    }
+
+    // Only ADMIN can create and delete holidays
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/create-public-holiday")
+    public ResponseEntity<SuccessResponse> createHoliday(@RequestBody PublicHolidayRequest publicHolidayRequest) {
+        publicHolidayService.createHoliday(publicHolidayRequest);
+        return ResponseEntity.ok(
+                new SuccessResponse("Public holiday created!")
+        );
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("/update-public-holiday/{id}")
+    public ResponseEntity<SuccessResponse> updateHoliday(@PathVariable Long id,
+                                                         @RequestBody PublicHolidayRequest request) {
+        publicHolidayService.updateHoliday(id, request);
+        return ResponseEntity.ok(
+                new SuccessResponse("Public holiday updated!")
+        );
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/delete-public-holiday/{id}")
+    public ResponseEntity<SuccessResponse> deleteHoliday(@PathVariable Long id) {
+        publicHolidayService.deleteHoliday(id);
+        return ResponseEntity.ok(
+                new SuccessResponse("Public holiday deleted!")
+        );
+    }
 }
