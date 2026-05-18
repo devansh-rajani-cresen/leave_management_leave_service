651c4600020a7790be2b22a73c83dc763b8aa469
DevanshCresen <devansh.rajani@cresensolutions.com>
2026-05-18 12:18:21 +0530

    feat: add JWT request utility for token extraction

--- src/main/java/com/cresensolutions/leaveservice/util/JwtRequestUtil.java
+++ src/main/java/com/cresensolutions/leaveservice/util/JwtRequestUtil.java
@@ -1,4 +1,19 @@
 package com.cresensolutions.leaveservice.util;
 
+import com.cresensolutions.leaveservice.exception.CustomException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import static com.cresensolutions.leaveservice.common.LeaveConstants.*;
+
+@Component
+@RequiredArgsConstructor
 public class JwtRequestUtil {
+    public String extractToken(HttpServletRequest request){
+        String authHeader = request.getHeader(AUTH_HEADER);
+        if (authHeader == null || !authHeader.startsWith(HEADER_STARTING)){
+            throw new CustomException("Invalid Token!", 400);
+        }
+        return authHeader.substring(TOKEN_STARTING_INDEX);
+    }
 }
