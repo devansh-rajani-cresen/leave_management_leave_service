d19b7d3767ecb0d45430f879afc99bbf82701bc0
DevanshCresen <devansh.rajani@cresensolutions.com>
2026-05-19 14:58:12 +0530

    feat: Feign client configuration added for inter microservices data fetch

--- src/main/java/com/cresensolutions/leaveservice/client/UserClient.java
+++ src/main/java/com/cresensolutions/leaveservice/client/UserClient.java
@@ -1,4 +1,28 @@
+// Internal AI APIs for chatbot tool calling
+
 package com.cresensolutions.leaveservice.client;
 
-public class UserClient {
+import com.cresensolutions.leaveservice.config.FeignConfig;
+import com.cresensolutions.leaveservice.dto.aidto.BasicUserInfoForAI;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
+
+@FeignClient(
+        name = "user-service",
+        url = "${user-service.url}",
+        configuration = FeignConfig.class
+)
+public interface UserClient {
+
+    @GetMapping("/internal/users/count")
+    Long getEmployeeCount();
+
+    @GetMapping("/internal/users/role/{role}")
+    List<BasicUserInfoForAI> getUserByRole(@PathVariable String role);
+
+    @GetMapping("/internal/users/search")
+    List<BasicUserInfoForAI> searchUsers(@RequestParam String name);
 }
--- src/main/java/com/cresensolutions/leaveservice/config/FeignConfig.java
+++ src/main/java/com/cresensolutions/leaveservice/config/FeignConfig.java
@@ -1,4 +1,27 @@
 package com.cresensolutions.leaveservice.config;
 
+import feign.RequestInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import java.util.Objects;
+import static com.cresensolutions.leaveservice.common.LeaveConstants.AUTH_HEADER;
+
+@Configuration
 public class FeignConfig {
+
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return requestTemplate -> {
+            String token = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder
+                    .getRequestAttributes()))
+                    .getRequest()
+                    .getHeader(AUTH_HEADER);
+
+            if (token != null) {
+                requestTemplate.header(AUTH_HEADER, token);
+            }
+        };
+    }
 }
