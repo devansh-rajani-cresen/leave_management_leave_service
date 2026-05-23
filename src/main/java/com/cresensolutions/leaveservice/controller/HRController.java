fa396d96521f39ff4a0072ef38f566057d899a0b
DevanshCresen <devansh.rajani@cresensolutions.com>
2026-05-23 14:51:01 +0530

    feat: Controllers added for HR and Manager

--- src/main/java/com/cresensolutions/leaveservice/controller/HRController.java
+++ src/main/java/com/cresensolutions/leaveservice/controller/HRController.java
@@ -1,4 +1,52 @@
 package com.cresensolutions.leaveservice.controller;
 
+import com.cresensolutions.leaveservice.dto.HRTaskResponse;
+import com.cresensolutions.leaveservice.dto.MyEmpLeaveHistory;
+import com.cresensolutions.leaveservice.service.HRService;
+import com.cresensolutions.leaveservice.util.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/hr")
+@AllArgsConstructor
 public class HRController {
+
+    private final HRService hrService;
+    private final JwtUtil jwtUtil;
+
+    @GetMapping("/my-tasks")
+    public List<HRTaskResponse> getHRTasks(HttpServletRequest request) {
+
+        // Extracting hrEmail from JWT token, instead of passing hrEmail in Query Param
+        String token = request.getHeader("Authorization").substring(7);
+        String hrEmail = jwtUtil.extractEmail(token);
+
+        log.info("Request came with Email of HR: {}", hrEmail);
+        return hrService.getHRTasks(hrEmail);
+    }
+
+    @PostMapping("/action")
+    public void handleHRAction(@RequestBody Map<String, Object> request,
+                               HttpServletRequest httpRequest) {
+        String authHeader = httpRequest.getHeader("Authorization");
+        String token = authHeader.substring(7);
+
+        String hrName = jwtUtil.extractfullName(token);
+        request.put("hrName", hrName);
+
+        hrService.handleHRAction(request);
+    }
+
+    // Returning all employees leave history along with trail (reducing API call)
+    @GetMapping("/emp-leave-history")
+    public List<MyEmpLeaveHistory> getAllLeaveHistory(){
+        return hrService.getAllLeaveHistory();
+    }
 }
--- src/main/java/com/cresensolutions/leaveservice/controller/ManagerController.java
+++ src/main/java/com/cresensolutions/leaveservice/controller/ManagerController.java
@@ -1,4 +1,51 @@
 package com.cresensolutions.leaveservice.controller;
 
+import com.cresensolutions.leaveservice.dto.MyEmpLeaveHistory;
+import com.cresensolutions.leaveservice.service.ManagerService;
+import com.cresensolutions.leaveservice.util.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/manager")
+@AllArgsConstructor
+@Slf4j
 public class ManagerController {
+
+    private final ManagerService managerService;
+    private final JwtUtil jwtUtil;
+
+    // Returns all employee-leaves to particular Manager by Manager EmailID
+    @GetMapping("/my-tasks")
+    public List<Map<String, Object>> getManagerTasks(HttpServletRequest request) {
+
+        // Extracting Manager Email from jwt token instead of passing in Query parameter
+        String authHeader = request.getHeader("Authorization");
+        String token = authHeader.substring(7);
+        String managerEmail = jwtUtil.extractEmail(token);
+        return managerService.getManagerTasks(managerEmail);
+    }
+
+    // Manager take action on employee-leave and Approve/Reject
+    @PostMapping("/action")
+    public void handleAction(@RequestBody Map<String, Object> request) {
+        log.info("Manager took action to approve/reject leave request: {}", request);
+        managerService.handleManagerAction(request);
+    }
+
+    // Returning individual Manager's employee leave history (by managerEmail) -> along with emp trail
+    @GetMapping("/my-emp-leave-history")
+    public List<MyEmpLeaveHistory> getMyEmployeeLeaveHistory(HttpServletRequest request){
+        String authHeader = request.getHeader("Authorization");
+        String token = authHeader.substring(7);
+        String managerEmail = jwtUtil.extractEmail(token);
+        log.info("Returning Employee's leave history for Manager: {}", managerEmail);
+        return managerService.getEmployeeLeaveHistory(managerEmail);
+    }
+
 }
